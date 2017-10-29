f090d5993e13efd2f79076c48f8ceecaf2721074
Matthew Hope <matthew.hope@blueyonder.co.uk>
2017-10-29 11:04:38 +0000

    added r10k to manage external puppet dependencies

--- provision.sh
+++ provision.sh
@@ -3,8 +3,11 @@
 # install puppet 5 yum repo
 sudo rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 
-# install puppet 5
-yum install puppet -y
+# install puppet 5, git and ruby
+yum install puppet ruby git -y
+
+# install r10k (old version as newer have dependencie issues)
+gem install r10k -v 2.1.1
 
 # replace /etc/puppet with the latest version
 if [[ -d /tmp/puppet ]]; then
@@ -19,5 +22,8 @@ if [[ -d /tmp/puppet ]]; then
 
 fi
 
+# bring in external puppet modules with r10k
+cd /etc/puppet; r10k puppetfile install
+
 # apply puppet
-puppet apply --confdir=/etc/puppet --verbose /etc/puppet/manifests/ --modulepath /etc/puppet/modules
+puppet apply --confdir=/etc/puppet --verbose /etc/puppet/manifests/ --modulepath /etc/puppet/modules:/etc/puppet/local-modules
