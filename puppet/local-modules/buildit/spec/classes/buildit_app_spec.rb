2ca161c6699a391f584ae9b6d5468e4cf6df075e
Matthew Hope <matthew.hope@blueyonder.co.uk>
2017-11-06 08:37:15 +0000

    added basic test coverage for classes

--- puppet/local-modules/buildit/spec/classes/buildit_app_spec.rb
+++ puppet/local-modules/buildit/spec/classes/buildit_app_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
  
 describe 'buildit::app', :type => 'class' do
+
+  app_group = 'test321'
+  app_user = 'test123'
+  app_repo_url = 'https://github.com/test/testrepo.git'
+  app_repo_revision = 'master'
+  app_directory = '/opt/nodeapp'
+  app_tcp_port = '2000'
+  node_repo_package_name = 'fakereleasee-release-el7-1'
+  node_repo_package_url = 'https://rpm.nodesource.com/pub_6.x/el/7/x86_64/fakereleasee-release-el7-1.noarch.rpm'
+  node_package = 'fakepackage'
+  systemd_template = 'buildit/nodejs-systemd'
+  systemd_unit_file = '/etc/systemd/system/test-app.service'
+  service_name = 'test-app'
     
   context "Should install app and open firewall" do
   	# set facts for test
@@ -16,21 +29,84 @@ describe 'buildit::app', :type => 'class' do
     }
     # set class params for test
     let(:params) { 
-      { :app_user  => 'test123',
-    	:app_group => 'test321',
-    	:app_repo_url => 'https://github.com/test/testrepo.git',
-    	:app_repo_revision => 'master'
+      { :app_user  => app_user,
+    	:app_group => app_group,
+    	:app_repo_url => app_repo_url,
+    	:app_repo_revision => app_repo_revision,
+      :app_directory => app_directory,
+      :app_tcp_port => app_tcp_port,
+      :node_repo_package_name =>  node_repo_package_name,
+      :node_repo_package_url => node_repo_package_url,
+      :node_package =>  node_package,
+      :systemd_template => systemd_template,
+      :systemd_unit_file => systemd_unit_file,
+      :service_name => service_name,
       } 
     }
     it do
-      should contain_group('test321').with(
+      should contain_group(app_group).with(
         'ensure' => 'present'
       )
-      should contain_user('test123').with(
+      should contain_user(app_user).with(
         'ensure'  => 'present',
-        'gid'    => 'test321',
+        'gid'     => app_group,
         'shell'   => '/sbin/nologin',
-        'require' => 'Group[test321]'
+        'require' => "Group[#{app_group}]"
+      )
+
+      should contain_package(node_repo_package_name).with(
+        'ensure'   => 'installed',
+        'provider' => 'rpm',
+        'source'   => node_repo_package_url,
+      )
+
+      should contain_package(node_package).with(
+        'ensure'  => 'installed',
+        'require' => "Package[#{node_repo_package_name}]",
+      )
+
+      should contain_vcsrepo(app_directory).with(
+        'ensure'   => 'latest',
+        'provider' => 'git',
+        'source'   => app_repo_url,
+        'revision' => app_repo_revision,
+        'require'  => "Package[#{node_package}]",
+      )
+
+      should contain_file(systemd_unit_file).with(
+        'owner'   => 'root',
+        'group'   => 'root',
+        'before'  => "Service[#{service_name}]",
+        'require' => "Vcsrepo[#{app_directory}]",
+        'notify'  => '[Exec[reload-systemd]{:command=>"reload-systemd"}, Service[test-app]{:name=>"test-app"}]',
+      )
+
+      should contain_exec('reload-systemd').with(
+        'command'     => 'systemctl daemon-reload',
+        'refreshonly' => true,
+      )
+
+      should contain_service(service_name).with(
+        'ensure'  => 'running',
+        'enable'  => true,
+        'require' => '[File[/etc/systemd/system/test-app.service]{:path=>"/etc/systemd/system/test-app.service"}, User[test123]{:name=>"test123"}]',
+      )
+
+      should contain_firewalld__custom_service('buildit-app').with(
+        'short'       => 'buildit-app',
+        'description' => 'Service for Buildit test Node.JS app',
+        'port'        => [
+            {
+                'port'     => app_tcp_port,
+                'protocol' => 'tcp',
+            }
+        ],
+      )
+
+      should contain_firewalld_service('Allow Access to Node.JS app from the public zone').with(
+        'ensure'  => 'present',
+        'service' => 'buildit-app',
+        'zone'    => 'public',
       )
     end
   end
--- puppet/local-modules/buildit/spec/classes/buildit_lb_spec.rb
+++ puppet/local-modules/buildit/spec/classes/buildit_lb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'buildit::lb', :type => 'class' do
     
   context "Should install apache lb and open firewall" do
-  	# set facts for test
+    # set facts for test
     let(:facts) { 
       { :osfamily => 'RedHat',
         :operatingsystem => 'CentOS',
@@ -16,12 +16,43 @@ describe 'buildit::lb', :type => 'class' do
     }
     # set class params for test
     let(:params) { 
-      { :app_nodes  => ['http://192.168.1.1:3000', 'http://192.168.1.2:3000'],
+      { :app_nodes  => ['http://192.168.1.1:3000', 'http://192.168.1.2:3000','http://192.168.1.3:3000'],
       } 
     }
     it do
       should contain_class('apache').with(
         'default_vhost' => 'false',
+      )
+
+      should contain_apache__balancermember('http://192.168.1.1:3000-buildit').with(
+        'balancer_cluster' => 'buildit',
+        'url'              => 'http://192.168.1.1:3000',
+      )
+
+      should contain_apache__balancermember('http://192.168.1.2:3000-buildit').with(
+        'balancer_cluster' => 'buildit',
+        'url'              => 'http://192.168.1.2:3000',
+      )
+
+      should contain_apache__balancermember('http://192.168.1.3:3000-buildit').with(
+        'balancer_cluster' => 'buildit',
+        'url'              => 'http://192.168.1.3:3000',
+      )
+
+      should contain_class("apache::vhosts").with(
+        'vhosts' => {
+            'buildit_vhost' => {
+                'docroot'    => '/var/www/html',
+                'port'       => '80',
+                'proxy_pass' => [ {'path' => '/', 'url' => 'balancer://buildit' } ]
+            },
+        },
+      )
+      
+      should contain_firewalld_service('Allow Access to HTTP from the public zone').with(
+        'ensure'  => 'present',
+        'service' => 'http',
+        'zone'    => 'public',
       )
     end
   end
